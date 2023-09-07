848678cde92b924d18c82d9e1294eb934643cc5c
RuthRojas <ruro122020@gmail.com>
2023-09-07 11:59:00 -0600

    line out steps

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById('create-task-form')
+  let submitInput = form.children[2]
+ /*
+  1. get the text input content
+  2. create an li element with the content from the text input
+  3. add a delete button, date due in the li element 
+  4. add the li element to the ul
+  5. based on the color selected, determine in what order
+     the task will go and change the background to that color
+  6. 
+  */
+
 });
